ba1dd63bb2673f380bde8b0f0f69355f33d0ffd6
Thalys001 <thalyssouza708@gmail.com>
2023-04-13 22:53:38 -0300

    chore: adding comments to improve readability

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ function converterBin2Dec() {
   let decimal = 0;
   let position = 0;
 
+  // check if the input contains only 0s and 1s
   if (/^[01]+$/.test(binary)) {
+    // loop through the binary digits and convert to decimal
     for (let i = binary.length - 1; i >= 0; i--) {
       decimal += parseInt(binary[i]) * Math.pow(2, position);
       position++;
     }
+    // display the decimal result
     document.getElementById("results").textContent =
       "The corresponding decimal number is: " + decimal;
   } else {
+    // display an error message if the input is not binary
     document.getElementById("results").textContent =
       "Invalid Input. Enter only 0 and 1.";
   }
